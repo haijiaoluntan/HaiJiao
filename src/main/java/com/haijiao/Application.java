5ba0473397f8f9ec917cbd3ba6604999d69ba42a
suikasa <2570005408@qq.com>
2019-08-26 21:44:33 +0800

    Application修改启动

--- src/main/java/com/haijiao/Application.java
+++ src/main/java/com/haijiao/Application.java
@@ -2,13 +2,13 @@ package com.haijiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("运行成功");
     }
-
 }
