0bd73a337704608f8c96bf63d4fa63305fb67ff7
suikasa <2570005408@qq.com>
2019-09-04 11:41:50 +0800

    预备实体类

---
+++ src/main/java/com/haijiao/pojo/Ready.java
@@ -0,0 +1,13 @@
+package com.haijiao.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ready {
+    private String phone;
+    private Integer yanzm;
+}
